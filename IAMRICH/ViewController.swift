145f89f1969197c1bbf792e34c423172c6b1d96b
Ed <pushpoped@gmail.com>
2021-10-13 16:32:09 -0400

    adding dynamic roll

--- IAMRICH/ViewController.swift
+++ IAMRICH/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+         
+    @IBOutlet weak var imageViewOne: UIImageView!
+    
+    @IBOutlet weak var imageViewTwo: UIImageView!
+    
+    
+    @IBAction func rollActionBtn(_ sender: Any) {
+        
+        print("Btn pressed")
+        let number = Int.random(in: 1..<7)
+        let number2 = Int.random(in: 1..<7)
+        
+        imageViewOne.image =  UIImage(named: String(number))
+        imageViewTwo.image =  UIImage(named: String(number2))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        imageViewOne.image =  UIImage(named: "2")
+        imageViewTwo.image =  UIImage(named: "1")
+        
+        
+        
     }
 
 
