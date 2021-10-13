fc5a70f24c87023e567fa3619f1da37a8d9d8a01
Ed <pushpoped@gmail.com>
2021-10-13 12:50:10 -0400

    Initial Commit

---
+++ IAMRICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAMRICH
+//
+//  Created by ed on 10/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
